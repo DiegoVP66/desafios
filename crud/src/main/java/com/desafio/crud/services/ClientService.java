b3e4188c1bec815f51de1abad786f80bee907fc0
DiegoVP66 <dvpdev33@gmail.com>
2022-03-02 12:00:25 -0300

    ClientService, findAll method created

---
+++ crud/src/main/java/com/desafio/crud/services/ClientService.java
@@ -0,0 +1,26 @@
+package com.desafio.crud.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.desafio.crud.dto.ClientDTO;
+import com.desafio.crud.entities.Client;
+import com.desafio.crud.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+
+	}
+}
