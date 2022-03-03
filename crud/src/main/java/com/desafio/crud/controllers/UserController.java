0d5253626c90f7be1a6bb6ab7e77dd701eb98c8a
DiegoVP66 <dvpdev33@gmail.com>
2022-03-03 18:01:11 -0300

    UserService, UserController, findAllPaged

---
+++ crud/src/main/java/com/desafio/crud/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.desafio.crud.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desafio.crud.dto.UserDTO;
+import com.desafio.crud.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+
+	@GetMapping
+	public ResponseEntity<Page<UserDTO>> findAllPaged(Pageable pageable) {
+		Page<UserDTO> dto = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(dto);
+	}
+
+}
