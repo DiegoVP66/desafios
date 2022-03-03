6d4626f20a9fcef70c1755ffece6f344072f4f4e
DiegoVP66 <dvpdev33@gmail.com>
2022-03-03 19:18:26 -0300

    Add spring-boot-starter-security dependency, BCryptPasswordEncoder, insert user

--- crud/src/main/java/com/desafio/crud/controllers/UserController.java
+++ crud/src/main/java/com/desafio/crud/controllers/UserController.java
@@ -1,15 +1,21 @@
 package com.desafio.crud.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.desafio.crud.dto.UserDTO;
+import com.desafio.crud.dto.UserInsertDTO;
 import com.desafio.crud.services.UserService;
 
 @RestController
@@ -29,6 +35,13 @@ public class UserController {
 	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
 		UserDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto) {
+		UserDTO newDTO = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDTO);
 	}
 
 }
--- crud/src/main/java/com/desafio/crud/services/UserService.java
+++ crud/src/main/java/com/desafio/crud/services/UserService.java
@@ -5,11 +5,16 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.desafio.crud.dto.RoleDTO;
 import com.desafio.crud.dto.UserDTO;
+import com.desafio.crud.dto.UserInsertDTO;
+import com.desafio.crud.entities.Role;
 import com.desafio.crud.entities.User;
+import com.desafio.crud.repositories.RoleRepository;
 import com.desafio.crud.repositories.UserRepository;
 import com.desafio.crud.services.exceptions.ResourceNotFoundException;
 
@@ -18,6 +23,12 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	@Transactional(readOnly = true)
 	public Page<UserDTO> findAllPaged(Pageable pageable) {
@@ -30,6 +41,24 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new UserDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public UserDTO insert(UserInsertDTO dto) {
+		User entity = new User();
+
+		entity.setEmail(dto.getEmail());
+		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+
+		entity.getRoles().clear();
+		for (RoleDTO roleDTO : dto.getRoles()) {
+			Role role = roleRepository.getById(roleDTO.getId());
+			entity.getRoles().add(role);
+		}
+
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+
 	}
 
 }
