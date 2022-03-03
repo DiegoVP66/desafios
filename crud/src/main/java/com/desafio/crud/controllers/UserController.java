94cb2e142ccfd5fd6e88c8250616a727f6587e7e
DiegoVP66 <dvpdev33@gmail.com>
2022-03-03 18:55:10 -0300

    Add findById

--- crud/src/main/java/com/desafio/crud/controllers/UserController.java
+++ crud/src/main/java/com/desafio/crud/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class UserController {
 	@GetMapping
 	public ResponseEntity<Page<UserDTO>> findAllPaged(Pageable pageable) {
 		Page<UserDTO> dto = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+		UserDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
 
--- crud/src/main/java/com/desafio/crud/services/UserService.java
+++ crud/src/main/java/com/desafio/crud/services/UserService.java
@@ -1,5 +1,7 @@
 package com.desafio.crud.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.desafio.crud.dto.UserDTO;
 import com.desafio.crud.entities.User;
 import com.desafio.crud.repositories.UserRepository;
+import com.desafio.crud.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,6 +23,13 @@ public class UserService {
 	public Page<UserDTO> findAllPaged(Pageable pageable) {
 		Page<User> page = repository.findAll(pageable);
 		return page.map(x -> new UserDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new UserDTO(entity);
 	}
 
 }
