38095047bf0947df90f4045a0ae776077ea03c5b
DiegoVP66 <dvpdev33@gmail.com>
2022-03-08 21:12:18 -0300

    Authorize requests

--- crud/src/main/java/com/desafio/crud/config/ResourceServerConfig.java
+++ crud/src/main/java/com/desafio/crud/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.desafio.crud.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -15,7 +16,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private JwtTokenStore tokenStore;
 
-	private static final String[] PUBLIC = { "/oauth/token", "/clients/**" };
+	private static final String[] PUBLIC = { "/oauth/token" };
+	private static final String[] OPERATOR_OR_ADMIN = { "/clients/**" };
+	private static final String[] ADMIN = { "/users/**" };
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -24,7 +27,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(PUBLIC).permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(PUBLIC).permitAll()
+		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
+		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN)
+				.permitAll()
+				.antMatchers(OPERATOR_OR_ADMIN)
+				.hasAnyRole("OPERATOR", "ADMIN")
+				.antMatchers(ADMIN)
+				.hasRole("ADMIN").anyRequest().authenticated();
 	}
 
 }
